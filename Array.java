78fcad5b0c3b78cf302639dca0bb837f5cf2f1d5
Karthik-2244 <yetukurikarthik63@gmail.com>
2024-11-23 07:35:47 -0500

    Add files via upload

--- Array.java
+++ Array.java
@@ -1,15 +1,33 @@
-public class Array
-{
-    public static void main(String args[])
-    {
-        int nums[] = new int[3];
-        nums [0]=4;
-        nums [1]=5;
-        nums[2]=7;
-        for(int i=0;i<3;i++)
-        {
-            System.out.println(nums[i]);
+
+//write a program to search the element in array?
+import java.util.Scanner;
+
+public class Array{
+    public static void main(String[] args) {
+        int count=0;
+        int result=0;
+        int nums[]= new int[25];
+        System.out.print("enter the numbers:");
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<25;i++){
+            nums[i]=sc.nextInt();
         }
+        System.out.println("enter the search element:");
+        int a = sc.nextInt();
+        for(int i=0;i<25;i++){
+            if(a==nums[i]){
+                count++;
+                result=result+1;
+            }
+
+        }
+        if(count>0){
+            System.out.println("number is there");
+
+            System.out.println("repeated time of number"+result);
+
+        }
+
 
     }
 }
